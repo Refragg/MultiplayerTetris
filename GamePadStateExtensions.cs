bcd14b959794b5308ad24473781acc965e573db2
Refrag <R3FR4GYT@gmail.com>
2021-09-29 23:05:41 +0200

    Extension methods for GamePadState to handle deadzones properly
    This allows to keep the same functionality while also supporting deadzones, I wish this was internally fixed because i don't think there's a better workaround other than changing the source code

---
+++ GamePadStateExtensions.cs
@@ -0,0 +1,54 @@
+using Microsoft.Xna.Framework.Input;
+
+namespace MultiplayerTetris
+{
+    public static class GamePadStateExtensions
+    {
+        public static bool IsButtonDown(this GamePadState state, Buttons stickButton, float leftDeadZone, float rightDeadZone)
+        {
+            switch (stickButton)
+            {
+                case Buttons.LeftThumbstickLeft:
+                case Buttons.LeftThumbstickRight:
+                case Buttons.LeftThumbstickDown:
+                case Buttons.LeftThumbstickUp:
+                case Buttons.RightThumbstickLeft:
+                case Buttons.RightThumbstickRight:
+                case Buttons.RightThumbstickDown:
+                case Buttons.RightThumbstickUp:
+                    return IsStickButtonPressed(state, stickButton, leftDeadZone, rightDeadZone);
+                default:
+                    return state.IsButtonDown(stickButton);
+            }
+        }
+        
+        public static bool IsStickButtonPressed(this GamePadState state, Buttons stickButton, float leftDeadZone, float rightDeadZone)
+        {
+            switch (stickButton)
+            {
+                case Buttons.LeftThumbstickLeft:
+                    return state.ThumbSticks.Left.X < -leftDeadZone;
+                case Buttons.LeftThumbstickRight:
+                    return state.ThumbSticks.Left.X > leftDeadZone;
+                
+                case Buttons.LeftThumbstickDown:
+                    return state.ThumbSticks.Left.Y > leftDeadZone;
+                case Buttons.LeftThumbstickUp:
+                    return state.ThumbSticks.Left.Y < -leftDeadZone;
+                
+                case Buttons.RightThumbstickLeft:
+                    return state.ThumbSticks.Right.X < -rightDeadZone;
+                case Buttons.RightThumbstickRight:
+                    return state.ThumbSticks.Right.X > rightDeadZone;
+                
+                case Buttons.RightThumbstickDown:
+                    return state.ThumbSticks.Right.Y < -rightDeadZone;
+                case Buttons.RightThumbstickUp:
+                    return state.ThumbSticks.Right.Y > rightDeadZone;
+                default:
+                    return false;
+            }
+        }
+    }
+    
+}
